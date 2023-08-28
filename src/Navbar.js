8de873778dde97f7b3c429ac659ba8d426993f43
Zo-ZerOs <62164947+Zo-ZerOs@users.noreply.github.com>
2023-08-28 13:33:05 +0900

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Navbar() {
-    return <header className="Navbar">Zo-ZerOs</header>;
+function Navbar({ feedClear }) {
+    return (
+        <div className="Navbar">
+            <header className="Navbar__title">Zo-ZerOs</header>
+            <button className="Feed__clear" onClick={() => feedClear()}>Clear</button>
+        </div>
+    );
 }
+
+Navbar.propTypes = {
+    feedClear: PropTypes.func.isRequired
+};
 
 export default Navbar;
